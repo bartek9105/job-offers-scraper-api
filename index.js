df041ee6837a94dd02a5ba5e48156a10a9ed01f5
bartek9105 <bartek9105@gmail.com>
2021-01-23 13:05:00 +0100

    Implement mongoose, create mongoose schema

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express')
 require('dotenv').config({ path: './config/.env' })
-const dbConnection = require('./config/dbConnection')
-
-dbConnection()
+const mongoose = require('mongoose')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello')
+const dbConnection = async () => await mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+
+dbConnection()
+
+const Technology = mongoose.model('Technology', { technologies: Array })
+
+app.get('/api/v1/offers', async (req, res) => {
+  try {
+    const offers = await Technology.find()
+    res.status(200).send(offers)
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
