01f6b6a38d821f3ed5a4444612561b71961cdf89
bartek9105 <bartek9105@gmail.com>
2021-01-24 13:49:03 +0100

    Add mongo query for getting offers count in cities

--- index.js
+++ index.js
@@ -16,11 +16,26 @@ const dbConnection = async () => await mongoose.connect(process.env.MONGO_URL, {
 
 dbConnection()
 
-const Offer = mongoose.model('Offer', { offers: Array }, 'technologies')
+const Offer = mongoose.model('Offer', { offers: Array }, 'offers')
 
 app.get('/api/v1/offers', async (req, res) => {
   try {
     const offers = await Offer.find()
+    res.status(200).send(offers)
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+// Get each city count
+app.get('/api/v1/offers/cities', async (req, res) => {
+  try {
+    const offers = await Offer.aggregate([{ $unwind: "$offers" }, {
+      $group: {
+        _id: "$offers.city",
+        count: { $sum: 1 }
+      }
+    }, { $sort: { count: -1 } }])
     res.status(200).send(offers)
   } catch (error) {
     console.error(error)
